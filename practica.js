2808d7c9b392b12b2e92536c38e5ca1e9bbefab5
nath-gallego <81245160+nath-gallego@users.noreply.github.com>
2021-06-21 18:53:56 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,30 @@
+// Mi primera practica con JavaScript.
+//Este es un comentario
+//Y este un comentario multilinea
+// Autor: Nathalie Gallego Ramirez
+const nombre = 'Nathalie Gallego' ;
+var tipo_doc = '1234567' ;
+let edad = '15' ;
+console.log (Hola '+nombre+' . Tu numero de documento es '+tipo_doc+'. Tienes: '+edad+');
+let anyos= "15"; 
+let comparacion = edad == anyos;
+console.log (comparacion);
+console.log (edad+anyos);
+console.log (edad+otro);
+console.log (otro>=edad);
+
+// 01/11/2005
+let suma = 0;
+suma = suma + 1;
+console.log(suma);
+suma = suma + 0;
+console.log(suma);
+
+suma = suma + 0;
+console.log(suma);
+
+var num1=0; num2=0;
+parseInt(prompt("Un cordial saludo, para ingresar un numero dale a la tecla enter"));
+num1=(prompt("ingresa un numero"));
+num2=(prompt("ingresa otro numero"));
+alert (num1+num2) ;
